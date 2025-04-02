fd700135410b599d640bac23ff3f4ba6476c8d7d
jesus suarez <suarezjesu90@gmail.com>
2025-04-02 17:19:56 -0300

    detalles

--- src/public/js/registro.js
+++ src/public/js/registro.js
@@ -3,5 +3,5 @@ const inicio = document.querySelector("#inicio")
 
 inicio.addEventListener("click",(e)=>{
 
-    window.location.href = process.env.route 
+    window.location.href = 'https://chat-usuarios-23dg.onrender.com'
 }) 
